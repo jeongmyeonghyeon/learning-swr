0cb2f894e82183a2c6bfe27550f592e2a5c84520
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-03 00:04:49 +0900

    interval 확인용 fetcher log 추가

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+const fetcher = (url) =>
+  axios.get(url).then((res) => {
+    console.log("fetcher is run");
+    return res.data;
+  });
 
 export default fetcher;
